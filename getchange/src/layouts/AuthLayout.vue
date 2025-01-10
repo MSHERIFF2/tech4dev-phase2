5bd8a5bb6e60e398a53b912333d9e2767767e581
Sheriffdeen Muritala Adegoke <muritalasheriffdeenadegoke@gmail.com>
2025-01-10 14:35:09 +0000

    updated the pictures part

--- getchange/src/layouts/AuthLayout.vue
+++ getchange/src/layouts/AuthLayout.vue
@@ -1,17 +1,32 @@
 <template>
-    <div class="flex">
-        <div class=" w-2/5 h-full reltive">
-            <div class=" absolute inset-0">
-                <img :src="require(`@/assets/images/${images[0]}`)" alt="male-user" >
-            </div>       
-                 
+    <div class="flex w-screen h-screen">
+        <div class=" w-2/5 h-full relative">
+            <img :src="currentImageSrc" alt="currentImage" class="h-full w-full object-cover absolute inset-0 z-0" />
+            <div class=" inset-0 z-20 absolute ">
+                <div class=" w-full h-full flex flex-col justify-end pb-14 text-white items-center">
+                    <div class="font-semibold text-xl">No Hazzles</div>
+                    <div class="w-3/5 font-light text-l text-center my-2">Lorem ipsum dolor sit amet, consectetur
+                        adipiscing
+                        elit, sed do eiusmod.</div>
+                    <div class="flex  items-center space-x-4">
+                        <div class="w-5 h-5 flex items-center justify-center border border-gray-400  rounded-full"
+                            v-for="image in images" :key="image">
+                            <button class="w-2.5 h-2.5 rounded-full border"
+                                @click="currentImage = image "></button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class=" w-3/5 flex justify-center items-center ">
-            <div></div>
+        <div class=" w-3/5 flex justify-center items-center  p-6">
+            <div>
+
+            </div>
             <slot />
-            <div class="text-gray-700">
-                <div>By siging up, you agree to our <a class="text-green-500 font-bold">Terms</a> and Privacy <a class="text-green-500 font-bold">Policy</a></div>
-                <div>© 2019 Tinylabs. All rights reserved.</div>
+            <div class="text-gray-700 flex justify-between text-sm font-light">
+                <div class="">By siging up, you agree to our <a class="text-green-500 font-semibold">Terms</a> and
+                    Privacy <a class="text-green-500 font-semibold">Policy</a></div>
+                <div class="">© 2019 Tinylabs. All rights reserved.</div>
             </div>
         </div>
     </div>
@@ -21,7 +36,32 @@ export default {
     name: 'AuthLayout',
     data() {
         return {
-            images: ['male.png', 'female.png']
+            currentImage: 'male.png',
+            images: ['male.png', 'female.png'],
+            intervalId: null
+        };
+    },
+    computed: {
+        currentImageSrc() {
+            return (require(`@/assets/images/${this.currentImage}`))
+        }
+    },
+    methods: {
+        setImageRotation() {
+            let currentIndex = this.images.indexOf(this.currentImage)
+            this.intervalId = setInterval(() => {
+                currentIndex = (currentIndex + 1) % this.images.length;
+                this.currentImage = this.images[currentIndex]
+                
+            }, 3000);
+        }
+    },
+    mounted() {
+        this.setImageRotation()
+    },
+    beforeDestroy() {
+        if (this.intervalId){
+            clearInterval(this.intervalId)
         }
     }
 }
