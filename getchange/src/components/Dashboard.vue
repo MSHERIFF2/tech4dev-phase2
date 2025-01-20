8969b4a9276c8323e680e4a5e065173ab28e025b
Sheriffdeen Muritala Adegoke <muritalasheriffdeenadegoke@gmail.com>
2025-01-20 15:01:25 +0000

    updated

--- getchange/src/components/Dashboard.vue
+++ getchange/src/components/Dashboard.vue
@@ -1,42 +1,60 @@
 <template>
-<div class="container flex flex-col w-full h-full">
-    <div class="top w-['100%'] flex-col  mt-4">
-        <div class="flex justify-between items-center w-2xl m-4 bg-white px-2 py-1 rounded">
-            <span class="text-[#013C61] text-xl">Store Wallet</span>
-            <span class=" font-bold text-xl text-[#013C61]"> ₦ 2,500
+    <div class="container flex flex-col w-full h-full">
+        <div class="top w-['100%'] flex-col  mt-4">
+            <div class="flex justify-between items-center w-2xl m-4 bg-white px-2 py-1 rounded">
+                <span class="text-[#013C61] text-xl">Store Wallet</span>
+                <span class=" font-bold text-xl text-[#013C61]"> ₦ 2,500
                 </span>
                 <button class="p-3 text-white bg-green-500">Top up wallet</button>
-        </div>
-        <div class="flex justify-between items-center w-2xl m-4 bg-white px-2 py-1 rounded">
-            <span class=" text-xl text-[#013C61]">Total Disbursed</span>
-            <span class=" font-bold text-xl text-[#013C61]"> ₦
-                12,000
+            </div>
+            <div class="flex justify-between items-center w-2xl m-4 bg-white px-2 py-1 rounded">
+                <span class=" text-xl text-[#013C61]">Total Disbursed</span>
+                <span class=" font-bold text-xl text-[#013C61]"> ₦
+                    12,000
                 </span>
                 <button class="p-3 text-gray-500 border">View History</button>
+            </div>
+
         </div>
-        
-    </div>
-    <div class="middl w-full p-6">
-        <div class="wrapper text-[#013C61] flex justify-between items-center w-full">
-            <span>Recent Activities</span>
-            <div class="flex justify-around items-center gap-x-3">
-                <button>1</button>
-                <span>of</span>
-                <span>8</span>
+        <div class="middl w-full p-6">
+            <div class="wrapper text-[#013C61] flex justify-between items-center w-full">
+                <span>Recent Activities</span>
                 <div class="flex justify-around items-center gap-x-3">
-                    <button class="text-xs rounded-full w-4 h-4 bg-[#2BDA53] text-[#FFFFFF]">
-                        <
-                    </button>
-                    
-                    <button class=" text-xs rounded-full w-4 h-4 bg-[#2BDA53] text-[#FFFFFF]">
-                        >
-                    </button>
-                    
+                    <button>1</button>
+                    <span>of</span>
+                    <span>8</span>
+                    <div class="flex justify-around items-center gap-x-3">
+                        <button class="text-xs rounded-full w-4 h-4 bg-[#2BDA53] text-[#FFFFFF]">
+                            < </button>
+
+                                <button class=" text-xs rounded-full w-4 h-4 bg-[#2BDA53] text-[#FFFFFF]">
+                                    >
+                                </button>
+
+                    </div>
                 </div>
             </div>
         </div>
+        <div class="bottom p-6">
+            <table class="w-full">
+                <thead class="w-full">
+                    <tr class="w-full flex justify-between items-center gap-x-4">
+                        <th>#</th>
+                        <th>Description</th>
+                        <th>Date</th>
+                        <th>Time</th>
+                        <th>Staff</th>
+                        <th>Amount</th>
+                    </tr>
+                </thead>
+                <tbody class="w-full">
+                    <tr class="w-full flex justify-between items-center gap-x-4">
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </div>
-</div>
 </template>
 <script>
 export default {
