dbb4635853d04c761d55bb45c50c8ec04d78980c
Sheriffdeen Muritala Adegoke <muritalasheriffdeenadegoke@gmail.com>
2025-02-02 10:33:37 +0000

    new project title lezada_clone

---
+++ lezada_clone/src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="home">
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<script setup>
+import {ref} from 'vue'
+const message = ref('Welcome to lezada_clone')
+</script>
