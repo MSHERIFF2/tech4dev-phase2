ac36901b5fc7b3e7fa78eb64e27f9e86988ad86e
Sheriffdeen Muritala Adegoke <muritalasheriffdeenadegoke@gmail.com>
2025-02-16 00:35:55 +0000

    update my component

--- lezada_clone/src/views/HomeView.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="p-6 bg-gray-500 w-full h-full text-black">
-    <h1 class="text-green-400">{{ message }}</h1>
-  </div>
-</template>
-<script setup>
-import {ref} from 'vue'
-const message = ref('Welcome to lezada_clone')
-</script>
